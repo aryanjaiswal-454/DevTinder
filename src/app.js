97ab8ff4f1d05bcd78ba5e8eaa75d28ddb7f96d7
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-01-05 01:39:46 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({"name":"Aryan", "age":21});
+})
+app.put("/user",(req,res)=>{
+    res.send("User data saved successfully")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
 app.use("/test",(req,res)=>{
     res.send("Hello from express server");
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello World!")
-})
-app.use("/",(req,res)=>{
-    res.send("Hello home page!");
-})
+
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
-});
+}); 
