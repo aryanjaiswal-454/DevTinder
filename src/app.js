d5b691e05e9cff012898ebf8a8f82d7fd0545e7b
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-01-05 14:06:20 +0530

    Middlewares

--- src/app.js
+++ src/app.js
@@ -2,16 +2,34 @@ const express = require("express");
 
 const app = express();
 
-// app.get("/user",(req,res)=>{
-//     console.log(req.query)
-//     res.send({"firstName":"Aryan", "lastName":"Jaiswal"});
-// })
+app.get(
+    "/user",
+    [(req,res,next)=>{
+        console.log("1st response sended");
+        // res.send("Route Handler 1");
+        next();  // This will give error because response is already sent but not in the last one because in last one it's acceptable
+    },
+    (req,res,next)=>{
+        console.log("2nd response sended");
+        // res.send("Route Handler 2");
+        next();
+    }],
+    [(req,res,next)=>{
+        console.log("3rd response sended");
+        res.send("Route Handler 3");
+        next();
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params)
-    res.send({"firstName":"Aryan", "lastName":"Jaiswal"});
-})
+    }],
+    (req,res,next)=>{
+        console.log("4th response sended");
+        // res.send("Route Handler 4"); 
+        next();  // This will not give error even after sending response because it's the last middleware
+    }
+)
+
+// Middleware -  function that has access to req, res and next objects
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
-}); 
+});   
