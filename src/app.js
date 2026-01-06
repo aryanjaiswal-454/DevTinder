0285d76799da19c04e6a1cbe6b16c9099bae68cf
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-01-07 00:38:45 +0530

    Error-Handling

--- src/app.js
+++ src/app.js
@@ -2,28 +2,33 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth,userAuth} = require("./middlewares/auth")
 
-// handle auth middleware for all GET, POST, DELETE ...requests
-app.use("/admin", adminAuth)
-
-app.post("/user/login",(req,res)=>{
-    res.send("Logged in successfully");
-})
-app.get("/user", userAuth, (req,res)=>{
-    res.send("User data sent");
-})
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All data from admin");
-})
-app.delete("/admin/deleteUser",(req,res)=>{
-    res.send("User deleted");
+app.use("/", (err,req,res,next)=>{
+    if(err){
+        console.log(err);
+        res.status(500).send("Something went wrong");
+    }
 })
 
+app.get("/getUserData",(req,res)=>{
+    try{
+        //logic of DB call and getting user data
+        throw new Error("hhddddj");
+        res.send("User data sent");
+    }
+    catch(err){
+        res.status(500).send("Error occurred");
+    }
+})
+
+app.use("/", (err,req,res,next)=>{  // should always be written at the end for best practices
+    if(err){
+        console.log(err);
+        res.status(500).send("Something went wrong");
+    }
+})
 
 
-
-
-app.listen(3000,()=>{
+app.listen(3000,()=>{ 
     console.log("Server is running on port 3000");
 });   
