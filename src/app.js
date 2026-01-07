41fc47a0cfd36604ed4f23fe670b64b9f7a6d2a8
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-01-07 16:08:39 +0530

    signup-user-dynamically

--- src/app.js
+++ src/app.js
@@ -5,22 +5,23 @@ const User = require("./models/user.js")
 
 const app = express();
 
-
+app.use(express.json());
 app.post("/signup",async (req,res)=>{
-    const userObj = {
-        firstName : "Rohit",
-        lastName : "Sharma",
-        emailId : "rohit@bcci.com",
-        password : "worldcup2027",
-        age : 20,
-        gender : "male"
-    }
+    // const userObj = {
+    //     firstName : "Rohit",
+    //     lastName : "Sharma",
+    //     emailId : "rohit@bcci.com",
+    //     password : "worldcup2027",
+    //     age : 20,
+    //     gender : "male"
+    // }
+    const userObj = req.body
     try{
         const user = new User(userObj);
         await user.save();
         res.send("User added successfully");
     } catch(err){
-        res.status(400).send("Error occurred", res.message)
+        res.status(400).send("Error occurred", err.message)
     }
 })
 
