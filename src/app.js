17f39969ae8ab377fe493aa9c678ce6b1e51deb6
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-02-26 02:19:48 +0530

    refactoring

--- src/app.js
+++ src/app.js
@@ -37,95 +37,15 @@ app.post("/signup",async (req,res)=>{
         res.status(400).send("ERROR : "+err.message)
     }
 })
-app.get("/profile",userAuth, async (req,res)=>{
-    try{
-        res.send(req.user);
-    }
-    catch(err){
-        res.status(401).send("ERROR : "+err.message);
-    }
-})
-app.get("/user", async (req,res)=>{
-    const userEmailId = req.body.emailId;
-
-    try{
-        const user = await User.findOne({emailId:userEmailId});  // a single object that created first (oldest)
-        if(!user) res.status(404).send("User not found")
-        else res.send(user);
-    } catch(err){
-        res.status(400).send("Something went wrong");
-    }
-
-
-    // try{
-    //     const users = await  User.find({emailId:userEmailId});   // array of objects
-    //     if(users.length===0) res.status(404).send("User not found");
-    //     else res.send(users);
-    // } catch(err){
-    //     res.status(400).send("Something went wrong");
-    // }
-
-})
-app.get("/feed", async (req,res)=>{
-    try{
-        const users = await  User.find({});
-        if(users.length===0) res.status(404).send("User not found");
-        else res.send(users);
-    } catch(err){
-        res.status(400).send("Something went wrong");
-    }
-})
-
-app.delete("/user", async (req,res)=>{
-    const userId = req.body.userId;
-    try{
-        const user = await User.findByIdAndDelete(userId);
-        if(!user) res.send("User doesn't exist")
-        else res.send("User deleted successfully");
-    }
-    catch(err){
-        res.status(400).send("Something went wrong");
-    }
-})
-
-app.patch("/user/:userId",async (req,res)=>{
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    
-    try{
-        const ALLOWED_UPDATES = ["age","gender","about","photoUrl","skills"]
-        const isUpdateAllowed = Object.keys(data).every((k)=>
-            ALLOWED_UPDATES.includes(k)
-        )
-        if(!isUpdateAllowed){
-            throw new Error("Update not allowed");
-        }
-        if(data?.skills.length>10){
-            throw new Error("Too much skills...MAX LENGTH is 10")
-        }
-        const a = await User.findByIdAndUpdate({_id : userId}, data , {
-            returnDocument : "before", 
-            runValidators : true
-        } );
-        console.log(a);
-        res.send("User updataed successfully")
-    }
-    catch(err){
-        res.status(401).send(err.message);
-    }
-})
-
 app.post("/login",async (req,res)=>{
     try{
         const {emailId, password} = req.body;
         if(!validator.isEmail(emailId)) throw new Error("Invalid credentials");
         const user = await User.findOne({emailId:emailId});
         if(!user) throw new Error("Invalid credentials");
-        const userPassword = user.password;
-        const isPasswordValid = await bcrypt.compare(password,userPassword);
+        const isPasswordValid = await user.validatePassword(password);
         if(isPasswordValid){
-            const token = jwt.sign({_id:user._id},"DEV@Tinder$790");
+            const token = await user.getJWT();
             res.cookie("token",token);
             res.send("Login successfull");
         }
@@ -135,6 +55,26 @@ app.post("/login",async (req,res)=>{
         res.status(401).send("ERROR : "+err.message);
     }
 })
+app.get("/profile",userAuth, async (req,res)=>{
+    try{
+        res.send(req.user);
+    }
+    catch(err){
+        res.status(401).send("ERROR : "+err.message);
+    }
+})
+app.post("/sendConnectionRequest",userAuth, (req,res)=>{
+    try{
+        const user = req.user;
+        console.log("Connection request sent successfully");
+        res.send(user.firstName+" "+user.lastName+" sent you a connection request");
+    }
+    catch(err) {
+        res.status(401).send("ERROR : "+err.message);
+    }
+})
+
+
 
 // NEVER TRUST req.body BECAUSE IT CAN GET MANY MALICIOUS THINGS IN YOUR DATABASE
 
