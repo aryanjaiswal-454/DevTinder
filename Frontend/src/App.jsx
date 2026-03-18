4ad425684948e28eb9e8835102dd2f4e6a6b24dc
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-18 18:54:21 +0530

    Basic routing with header and footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import NavBar from "./NavBar.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body.jsx"
+import Login from "./Login.jsx"
+import Profile from "./Profile.jsx"
 function App() {
   return (
     <>
-      <NavBar />
-      <h1>Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
