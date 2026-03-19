c728b5745ddc1efcc86f4449fd8ea5bf1b80b8eb
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-19 14:28:42 +0530

    Store and slice

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Body.jsx"
 import Login from "./Login.jsx"
 import Profile from "./Profile.jsx"
+import appStore from "./utils/appStore.js";
+import { Provider } from "react-redux";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body/>}>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,21 +1,25 @@
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios"
+import {useDispatch} from "react-redux"
+import {addUser} from "./utils/userSlice"
 const Login = () => {
     const [emailId, setEmailId] = useState("aryan@gmail.com");
     const [password, setPassword] = useState("Aryan@123");
-
-    const handleLogin = async()=>{
-        try {const res = await axios.post("http://localhost:3000/login",
-            {
-                emailId,
-                password,
-            },
-            {
-                withCredentials: true,
-            }
-        );
-        console.log(res);}
-        catch(err){
+    const dispatch = useDispatch();
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:3000/login",
+                {
+                    emailId,
+                    password,
+                },
+                {
+                    withCredentials: true,
+                }
+            );
+            dispatch(addUser(res.data))
+        }
+        catch (err) {
             console.log(err);
         }
     }
@@ -27,20 +31,20 @@ const Login = () => {
                     <div>
                         <fieldset className="fieldset my-3">
                             <legend className="fieldset-legend my-1 font-normal text-base">Email ID</legend>
-                            <input type="email" value={emailId} className="input input-bordered w-full px-3" 
-                            onChange={(e)=> setEmailId(e.target.value)}
+                            <input type="email" value={emailId} className="input input-bordered w-full px-3"
+                                onChange={(e) => setEmailId(e.target.value)}
                             />
                         </fieldset>
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend my-1 font-normal text-base">Password</legend>
-                            <input type="text" value={password} className="input input-bordered w-full px-3"  
-                            onChange={(e)=> setPassword(e.target.value)}
+                            <input type="text" value={password} className="input input-bordered w-full px-3"
+                                onChange={(e) => setPassword(e.target.value)}
                             />
                         </fieldset>
                     </div>
                     <div className="card-actions justify-center py-3">
                         <button className="px-4 py-2 rounded-lg bg-blue-500 text-white shadow-md shadow-blue-500/40 hover:bg-blue-600 transition"
-                        onClick={handleLogin}>
+                            onClick={handleLogin}>
                             Login
                         </button>
                     </div>
