35a18aad252169d916eb454df6b0802d51c482c2
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-24 23:54:46 +0530

    debug ui

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,31 +1,29 @@
 /* eslint-disable no-unused-vars */
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios'
-import { BASE_URL } from '../utils/constants'
-import {removeUser} from '../utils/userSlice.js'
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice.js";
 const NavBar = () => {
   const user = useSelector((store) => store.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleLogout = async () => {
     try {
-      const res = await axios.post(BASE_URL + "/logout",
-        {
-        },
+      const res = await axios.post(
+        BASE_URL + "/logout",
+        {},
         {
           withCredentials: true,
-        }
-      )
+        },
+      );
       dispatch(removeUser());
-      alert("You have been logged out")
-      return navigate("/login")
-
+      alert("You have been logged out");
+      return navigate("/login");
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
+  };
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
@@ -44,21 +42,21 @@ const NavBar = () => {
                 className="btn btn-ghost btn-circle avatar"
               >
                 <div className="w-10 h-10 rounded-full overflow-hidden">
-  <img
-    src={user.photoUrl}
-    alt="profile"
-    className="w-full h-full object-cover"
-  />
-</div>
+                  <img
+                    src={user.photoUrl}
+                    alt="profile"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
               </div>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-200/95 backdrop-blur-md rounded-2xl z-[1] mt-3 w-52 p-2 shadow-xl border border-white/10"
+                className="menu menu-sm dropdown-content bg-base-300  rounded-2xl z-[1] mt-3 w-52 p-2 shadow-xl border border-white/10"
               >
                 <li>
                   <Link
                     to="/profile"
-                    className="justify-between rounded-lg hover:bg-base-300 transition-all duration-200 px-3 py-2"
+                    className="flex items-center justify-between rounded-lg px-3 py-2 transition-all duration-200 hover:bg-white/10 hover:scale-[1.03] hover:shadow-lg"
                   >
                     Profile
                     <span className="badge badge-primary badge-sm">New</span>
@@ -66,13 +64,20 @@ const NavBar = () => {
                 </li>
 
                 <li>
-                  <a className="rounded-lg hover:bg-base-300 transition-all duration-200 px-3 py-2">
+                  <a
+                    className="rounded-lg px-3 py-2 transition-all duration-200 
+                    hover:bg-white/10 hover:backdrop-blur-md 
+                    hover:shadow-lg hover:scale-[1.02]"
+                  >
                     Settings
                   </a>
                 </li>
 
                 <li>
-                  <a onClick={handleLogout} className="rounded-lg text-error hover:bg-error/10 transition-all duration-200 px-3 py-2">
+                  <a
+                    onClick={handleLogout}
+                    className="rounded-lg text-error hover:bg-error/10 transition-all duration-200 px-3 py-2"
+                  >
                     Logout
                   </a>
                 </li>
